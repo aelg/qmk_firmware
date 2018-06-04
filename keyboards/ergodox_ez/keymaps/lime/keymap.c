74f91b3850db1696dbb069ef62086ba597a79991
Linus Mellberg <linus.mellberg@gmail.com>
2018-06-04 22:57:45 +0200

    Formatting

--- keyboards/ergodox_ez/keymaps/lime/keymap.c
+++ keyboards/ergodox_ez/keymaps/lime/keymap.c
@@ -9,8 +9,8 @@
 //#include "keymap_nordic.h"
 
 
-#define BASE 0 // default layer
-#define COLEMAK 1 // default layer
+#define BASE 0 // qwerty layer
+#define COLEMAK 1 // colemak layer
 #define SYMB 2 // symbols
 #define MOUSE 3 // media keys
 #define XMONAD 4 // media keys
@@ -52,13 +52,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [COLEMAK] = LAYOUT_ergodox(
     // left hand
     KC_EQUAL,        KC_EXLM,      KC_AT,            KC_HASH,         KC_DLR,         KC_PERC,       DF(BASE),
-    KC_DELETE,       KC_Q,         KC_W,             KC_F,            KC_P,           KC_G,          TG(SYMB),
+    KC_DELETE,       KC_Q,         KC_W,             KC_F,            KC_P,           KC_G,          KC_ESCAPE,
     KC_BSPACE,       KC_A,         LT(NAV,KC_R),     LT(MOUSE,KC_S),  LT(SYMB,KC_T),  KC_D,
     KC_LSHIFT,       CTL_T(KC_Z),  KC_X,             KC_C,            KC_V,           KC_B,          ALL_T(KC_NO),
     LT(SYMB,KC_GRAVE),  KC_QUOTE,     LALT(KC_LSHIFT),  KC_LEFT,     KC_RIGHT,     
-                                             ALT_T(KC_APPLICATION),  KC_LGUI,
-                                                                     KC_HOME,
-                                        LT(SYMB,KC_SPACE),  KC_DELETE,  KC_END,
+                                                                               ALT_T(KC_APPLICATION),  KC_LGUI,
+                                                                                                       KC_HOME,
+                                                                        LT(SYMB,KC_SPACE),  KC_DELETE,  KC_END,
     
                                         
     // right hand
@@ -66,18 +66,59 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TG(SYMB),        KC_J,         KC_L,             KC_U,            KC_Y,             KC_SCOLON,         KC_BSLASH,
                      KC_H,         KC_N,             KC_E,            KC_I,             LT(MOUSE,KC_O),    GUI_T(KC_QUOTE),
     MEH_T(KC_NO),    KC_K,         KC_M,             KC_COMMA,        KC_DOT,           RCTL_T(KC_SLASH),  KC_RSHIFT,
-                                   KC_DOWN,          KC_UP,           KC_LBRACKET,      KC_RBRACKET,MO(SYMB),
+                                   KC_DOWN,          KC_UP,           KC_LBRACKET,      KC_RBRACKET,       MO(SYMB),
     KC_LALT,    CTL_T(KC_ESCAPE),
     KC_PGUP,
     KC_PGDOWN,  KC_TAB,  LT(XMONAD,KC_ENTER)
 ),
-  [SYMB] = LAYOUT_ergodox(KC_ESCAPE,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_TRANSPARENT,KC_TRANSPARENT,KC_EXLM,KC_AT,KC_LCBR,KC_RCBR,KC_PIPE,KC_TRANSPARENT,KC_TRANSPARENT,KC_HASH,KC_DLR,KC_LPRN,KC_TRANSPARENT,KC_GRAVE,KC_TRANSPARENT,KC_PERC,KC_CIRC,KC_LBRACKET,KC_RBRACKET,KC_TILD,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,RGB_MOD,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,RGB_VAI,KC_TRANSPARENT,KC_TRANSPARENT,KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_TRANSPARENT,KC_UP,KC_7,KC_8,KC_9,KC_ASTR,KC_F12,KC_DOWN,KC_4,KC_5,KC_6,KC_PLUS,KC_TRANSPARENT,KC_TRANSPARENT,KC_AMPR,KC_1,KC_2,KC_3,KC_BSLASH,KC_TRANSPARENT,KC_TRANSPARENT,KC_DOT,KC_0,KC_EQUAL,KC_TRANSPARENT,RGB_TOG,RGB_SLD,KC_TRANSPARENT,KC_TRANSPARENT,RGB_HUD,KC_TRANSPARENT),
 
-  [MOUSE] = LAYOUT_ergodox(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_WH_LEFT,KC_MS_UP,KC_MS_WH_RIGHT,KC_MS_WH_UP,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_LEFT,KC_TRANSPARENT,KC_MS_RIGHT,KC_MS_WH_DOWN,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_BTN1,KC_MS_BTN2,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_WH_UP,KC_MS_WH_LEFT,KC_MS_UP,KC_MS_WH_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_WH_DOWN,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,KC_TRANSPARENT,KC_MEDIA_PLAY_PAUSE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_BTN1,KC_MS_BTN3,KC_MS_BTN2,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_WWW_BACK),
+[SYMB] = LAYOUT_ergodox(
+    // left hand
+    KC_ESCAPE,      KC_F1,         KC_F2,            KC_F3,           KC_F4,            KC_F5,           _______,
+    _______,        KC_EXLM,       KC_AT,            KC_LCBR,         KC_RCBR,          KC_PIPE,         _______,
+    _______,        KC_HASH,       KC_DLR,           KC_LPRN,         _______,          KC_GRAVE,
+    _______,        KC_PERC,       KC_CIRC,          KC_LBRACKET,     KC_RBRACKET,      KC_TILD,         _______,
+    _______,        _______,       _______,          _______,         _______,
+                                                                                RGB_MOD,     _______,
+                                                                                             _______,
+                                                                    _______,     RGB_VAI,    _______,
+                                                                    
+                                                                    
+    // right hand
+    _______,        KC_F6,         KC_F7,            KC_F8,           KC_F9,            KC_F10,          KC_F11,
+    _______,        KC_UP,         KC_7,             KC_8,            KC_9,             KC_ASTR,         KC_F12,
+                    KC_DOWN,       KC_4,             KC_5,            KC_6,             KC_PLUS,         _______,
+    _______,        KC_AMPR,       KC_1,             KC_2,            KC_3,             KC_BSLASH,       _______,
+                                   _______,          KC_DOT,          KC_0,             KC_EQUAL,        _______,
+    RGB_TOG,   RGB_SLD,
+    _______,
+    _______,   RGB_HUD,   _______
+),
 
-  [XMONAD] = LAYOUT_ergodox(KC_TRANSPARENT,LGUI(KC_1),LGUI(KC_2),LGUI(KC_3),LGUI(KC_4),LGUI(KC_5),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LGUI(KC_6),LGUI(KC_7),LGUI(KC_8),LGUI(KC_9),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LGUI(KC_H),LGUI(KC_J),LGUI(KC_K),LGUI(KC_L),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT),
+[MOUSE] = LAYOUT_ergodox(
+    _______,        _______,       _______,          _______,         _______,          _______,         _______,
+    _______,        _______,       KC_MS_WH_LEFT,    KC_MS_UP,        KC_MS_WH_RIGHT,   KC_MS_WH_UP,     _______,
+    _______,        _______,       KC_MS_LEFT,       _______,         KC_MS_RIGHT,      KC_MS_WH_DOWN,
+    _______,        _______,       _______,          _______,         _______,          _______,         _______,
+    _______,        _______,       _______,          KC_MS_BTN1,      KC_MS_BTN2,
+                                                                                   _______,   _______,
+                                                                                              _______,
+                                                                        _______,   _______,   _______,
 
-  [NAV] = LAYOUT_ergodox(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_PGUP,KC_HOME,KC_UP,KC_END,KC_TRANSPARENT,KC_TRANSPARENT,KC_PGDOWN,KC_LEFT,KC_DOWN,KC_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LGUI(KC_J),LGUI(KC_K),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LGUI(LSFT(KC_C)),LGUI(LSFT(KC_ENTER))),
+
+    _______,        _______,       _______,          _______,         _______,          _______,         _______,
+    _______,        KC_MS_WH_UP,   KC_MS_WH_LEFT,    KC_MS_UP,        KC_MS_WH_RIGHT,   _______,         _______,
+                    KC_MS_WH_DOWN, KC_MS_LEFT,       KC_MS_DOWN,      KC_MS_RIGHT,      _______,         KC_MEDIA_PLAY_PAUSE,
+    _______,        _______,       _______,          _______,         _______,          _______,         _______,
+                                   KC_MS_BTN1,       KC_MS_BTN3,      KC_MS_BTN2,       _______,         _______,
+    _______,    _______,
+    _______,
+    _______,    _______,    KC_WWW_BACK
+),
+
+  [XMONAD] = LAYOUT_ergodox(_______,LGUI(KC_1),LGUI(KC_2),LGUI(KC_3),LGUI(KC_4),LGUI(KC_5),_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,LGUI(KC_6),LGUI(KC_7),LGUI(KC_8),LGUI(KC_9),_______,_______,_______,_______,_______,_______,_______,_______,_______,LGUI(KC_H),LGUI(KC_J),LGUI(KC_K),LGUI(KC_L),_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______),
+
+  [NAV] = LAYOUT_ergodox(_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_PGUP,KC_HOME,KC_UP,KC_END,_______,_______,KC_PGDOWN,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______,_______,_______,_______,_______,_______,_______,_______,LGUI(KC_J),LGUI(KC_K),_______,_______,_______,_______,_______,_______,_______,LGUI(LSFT(KC_C)),LGUI(LSFT(KC_ENTER))),
 
 };
 
@@ -138,6 +179,8 @@ uint32_t layer_state_set_user(uint32_t state) {
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
+    layer = default_layer_state;
+     
     switch (layer) {
       case 0:
         #ifdef RGBLIGHT_COLOR_LAYER_0
